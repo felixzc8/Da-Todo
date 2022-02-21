412e213070320fa1309a0885d714517488d44d87
lucasyan420 <lucasyan2005@gmail.com>
2022-02-21 18:38:04 +0800

    21 Feb - Lucas: Made Pet Class

--- app/src/main/java/com/example/da_todo/Reward/Pet.java
+++ app/src/main/java/com/example/da_todo/Reward/Pet.java
@@ -2,4 +2,50 @@ package com.example.da_todo.Reward;
 
 public class Pet
 {
+    private String petName;
+    private int totalPoints;
+    private int petPoints;
+
+    public Pet()
+    {
+
+    }
+
+    public Pet(String petName, int totalPoints, int petPoints) {
+        this.petName = petName;
+        this.totalPoints = totalPoints;
+        this.petPoints = petPoints;
+    }
+
+    public String getPetName() {
+        return petName;
+    }
+
+    public void setPetName(String petName) {
+        this.petName = petName;
+    }
+
+    public int getTotalPoints() {
+        return totalPoints;
+    }
+
+    public void setTotalPoints(int totalPoints) {
+        this.totalPoints = totalPoints;
+    }
+
+    public int getPetPoints() {
+        return petPoints;
+    }
+
+    public void setPetPoints(int petPoints) {
+        this.petPoints = petPoints;
+    }
+
+    @Override
+    public String toString() {
+        return "Pet:" +
+                "\nPet Name = " + petName +
+                "\nTotal Points = " + totalPoints +
+                "\nPet Points = " + petPoints;
+    }
 }
