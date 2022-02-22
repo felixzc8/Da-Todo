f958c20512a08b3bd65112b15adcfd3ab7db0469
lucasyan420 <lucasyan2005@gmail.com>
2022-02-22 10:39:21 +0800

    22 Feb - Lucas: Updated UI for SignUp and SignIn

--- app/src/main/java/com/example/da_todo/Controllers/SignInActivity.java
+++ app/src/main/java/com/example/da_todo/Controllers/SignInActivity.java
@@ -2,7 +2,9 @@ package com.example.da_todo.Controllers;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.da_todo.R;
 
@@ -14,5 +16,10 @@ public class SignInActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+    }
+
+    public void goToSignUpActivity(View view){
+        Intent goToSignUpActivity = new Intent(this, SignUpActivity.class);
+        startActivity(goToSignUpActivity);
     }
 }
