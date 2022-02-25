126b4a14ee369d2a2a80985a5415dee6136e1ad7
felixchen <felixzc8@gmail.com>
2022-02-25 08:45:37 +0800

    22/2 - Felix - signIn and UI

--- app/src/main/java/com/example/da_todo/Controllers/SignInActivity.java
+++ app/src/main/java/com/example/da_todo/Controllers/SignInActivity.java
@@ -5,17 +5,49 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import com.example.da_todo.R;
+import com.example.da_todo.User.User;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class SignInActivity extends AppCompatActivity
 {
+    FirebaseAuth mAuth;
+    FirebaseFirestore firestore;
+
+    User user;
+
+    EditText emailInput, passwordInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        mAuth = FirebaseAuth.getInstance();
+        firestore = FirebaseFirestore.getInstance();
+
+        emailInput = findViewById(R.id.emailInputEditText);
+        passwordInput = findViewById(R.id.passwordInputEditText);
+    }
+
+    @Override
+    public void onStart()
+    {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if (currentUser != null)
+        {
+            String email = currentUser.getEmail();
+
+            System.out.println(String.format("Current User - email: %s", email));
+            goTaskActivity();
+        }
     }
 
     public void logIn(View v){
@@ -27,7 +59,8 @@ public class SignInActivity extends AppCompatActivity
         startActivity(goToSignUpActivity);
     }
 
-    public void goTaskActivity(){
+    public void goTaskActivity()
+    {
         Intent goToTasksActivity = new Intent(this, TasksActivity.class);
         startActivity(goToTasksActivity);
     }
