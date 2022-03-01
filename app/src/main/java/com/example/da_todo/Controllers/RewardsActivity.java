b8622213d0f0fcb004bb4e09e030ecce9e2d070a
lucasyan420 <lucasyan2005@gmail.com>
2022-03-01 10:49:27 +0800

    1 March - Lucas - Updated All UI

--- app/src/main/java/com/example/da_todo/Controllers/RewardsActivity.java
+++ app/src/main/java/com/example/da_todo/Controllers/RewardsActivity.java
@@ -2,7 +2,9 @@ package com.example.da_todo.Controllers;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import com.example.da_todo.R;
@@ -30,5 +32,17 @@ public class RewardsActivity extends AppCompatActivity
 
         petNameTextView = findViewById(R.id.petNameTextView);
         moneyTextView = findViewById(R.id.moneyTextView);
+    }
+
+    public void backButton(View v)
+    {
+        Intent backButton = new Intent(this, TasksActivity.class);
+        startActivity(backButton);
+    }
+
+    public void renamePetButton(View v)
+    {
+        Intent renamePetButton = new Intent(this, PetNamingActivity.class);
+        startActivity(renamePetButton);
     }
 }
