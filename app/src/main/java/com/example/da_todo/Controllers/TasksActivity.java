af46f916bbad5cef47c1f84d7ac4976c38ebe34c
felixchen <felixzc8@gmail.com>
2022-03-01 09:52:14 +0800

    1/3/22 - Felix - get user

--- app/src/main/java/com/example/da_todo/Controllers/TasksActivity.java
+++ app/src/main/java/com/example/da_todo/Controllers/TasksActivity.java
@@ -7,17 +7,30 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.da_todo.R;
 import com.example.da_todo.Reward.Pet;
 import com.example.da_todo.Task.Task;
+import com.example.da_todo.User.User;
 import com.example.da_todo.recyclerAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
 
 public class TasksActivity extends AppCompatActivity
 {
+    FirebaseAuth mAuth;
+    FirebaseFirestore firestore;
+    FirebaseUser mUser;
+
+    User user;
+
     private ArrayList<Task> taskList;
     private RecyclerView recyclerView;
     private RecyclerView recyclerAdapter;
@@ -28,13 +41,39 @@ public class TasksActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tasks_acvitiy);
+
+        mAuth = FirebaseAuth.getInstance();
+
         taskList = new ArrayList<>();
         recyclerView = findViewById(R.id.recyclerView_tasksActivity);
 
         userPet = (Pet) getIntent().getSerializableExtra("pet");
 
+        getUser();
         setTaskInfo();
         setAdapter();
+    }
+
+    public void getUser()
+    {
+        try
+        {
+            firestore.collection("/users").document(mUser.getUid()).get()
+                    .addOnCompleteListener(task ->
+                    {
+                        DocumentSnapshot ds = task.getResult();
+                        if (task.isSuccessful())
+                        {
+                            user = ds.toObject(User.class);
+                            Log.d("USER OBJECT", "user name: " + user.getUserName());
+                        }
+                    });
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            Toast.makeText(this, "error getting user", Toast.LENGTH_SHORT).show();
+        }
     }
 
     private void setAdapter() {
@@ -61,6 +100,14 @@ public class TasksActivity extends AppCompatActivity
         Intent goToRewardsActivity = new Intent(this, RewardsActivity.class);
         goToRewardsActivity.putExtra("pet", userPet);
         startActivity(goToRewardsActivity);
+    }
+
+    public void signOut(View v)
+    {
+        mAuth.signOut();
+        Intent intent = new Intent(this, SignInActivity.class);
+        startActivity(intent);
+        this.finish();
     }
 
 }
