281b5d8f9719d23113b11020a1afc822206d1729
felixchen <felixzc8@gmail.com>
2022-03-09 11:05:26 +0800

    9/3/22 - Felix - pet bugs

--- app/src/main/java/com/example/da_todo/Controllers/RewardsActivity.java
+++ app/src/main/java/com/example/da_todo/Controllers/RewardsActivity.java
@@ -15,8 +15,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 public class RewardsActivity extends AppCompatActivity
 {
     FirebaseFirestore firestore;
-    TextView petNameTextView;
-    TextView moneyTextView;
+    TextView petNameTextView, moneyTextView;
     User user;
     Pet pet;
 
@@ -34,11 +33,12 @@ public class RewardsActivity extends AppCompatActivity
         petNameTextView = findViewById(R.id.petName_TextView_RewardsActivity);
         moneyTextView = findViewById(R.id.money_TextView_RewardsActivity);
 
-        moneyTextView.setText(pet.getTotalPoints());
+        moneyTextView.setText(Integer.toString(pet.getTotalPoints()));
         petNameTextView.setText(pet.getName());
     }
 
-    public void feedPet(View v) {
+    public void feedPet(View v)
+    {
         int totalPoints = pet.getTotalPoints() - 50;
         int petPoints = pet.getPoints() + 50;
 
@@ -56,9 +56,10 @@ public class RewardsActivity extends AppCompatActivity
         startActivity(backButton);
     }
 
-    public void renamePetButton(View v)
+    public void renamePet(View v)
     {
-        Intent renamePetButton = new Intent(this, PetNamingActivity.class);
-        startActivity(renamePetButton);
+        Intent intent = new Intent(this, PetNamingActivity.class);
+        intent.putExtra("user", user);
+        startActivity(intent);
     }
 }
