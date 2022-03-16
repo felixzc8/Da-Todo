2695dcf3a441e4cc867574eac86f0fafc4a45f7c
Felix Chen <felixzc8@gmail.com>
2022-03-16 13:02:31 +0800

    16/3/21 - Felix - Lucas u silly little goose smh #2

--- app/src/main/java/com/example/da_todo/Controllers/AddTaskActivity.java
+++ app/src/main/java/com/example/da_todo/Controllers/AddTaskActivity.java
@@ -34,20 +34,17 @@ import com.google.firebase.storage.UploadTask;
 
 import java.util.UUID;
 
-public class AddTaskActivity extends AppCompatActivity
-{
+public class AddTaskActivity extends AppCompatActivity {
     private FirebaseFirestore firestore = FirebaseFirestore.getInstance();
     private FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
     User user;
 
-    private EditText taskName;
-    private EditText taskTime;
-    private EditText taskPoints;
+    private EditText taskName, taskTime, taskPoints;
 
-    private String userID;
-    private String userEmail;
-    private String userPin;
+    private String userID, userEmail, userPin;
+
+    String nameString, timeString, rewardString;
 
     private ImageView taskPhoto;
     public Uri imageUri;
@@ -58,8 +55,7 @@ public class AddTaskActivity extends AppCompatActivity
     private StorageReference storageReference;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
 
@@ -67,7 +63,7 @@ public class AddTaskActivity extends AppCompatActivity
 
 
         Bundle intentInfo = getIntent().getExtras();
-        if(intentInfo != null){
+        if (intentInfo != null) {
             userID = intentInfo.getString("userID");
             userEmail = intentInfo.getString("parentEmail");
             userPin = intentInfo.getString("parentPin");
@@ -107,7 +103,6 @@ public class AddTaskActivity extends AppCompatActivity
 //        System.out.println(user.getID() + "Ttttt");
 
 
-
 //        firestore.collection("users").document(user.getID()).update("tasks",
 //                FieldValue.arrayUnion(task);
 
@@ -126,8 +121,7 @@ public class AddTaskActivity extends AppCompatActivity
                 }
             });
 
-    private void choosePicture()
-    {
+    private void choosePicture() {
         Intent intent = new Intent();
         intent.setType("image/*");
         intent.setAction(Intent.ACTION_GET_CONTENT);
@@ -135,85 +129,102 @@ public class AddTaskActivity extends AppCompatActivity
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if(resultCode == RESULT_OK && data != null && data.getData() != null)
-        {
+        if (resultCode == RESULT_OK && data != null && data.getData() != null) {
             imageUri = data.getData();
             taskPhoto.setImageURI(imageUri);
         }
     }
 
-    public void addTask(View view) {
-        try{
-            String nameString = taskName.getText().toString();
-            int timeInt = Integer.parseInt(taskTime.getText().toString());
-            int rewardInt = Integer.parseInt(taskPoints.getText().toString());
+    public boolean formValid()
+    {
+        nameString = taskName.getText().toString();
+        timeString = taskTime.getText().toString();
+        rewardString = taskPoints.getText().toString();
 
-            taskUUID = UUID.randomUUID().toString();
-
-            final ProgressBar progressBar = new ProgressBar(this, null, android.R.attr.progressBarStyleLarge);
-            progressBar.setVisibility(View.VISIBLE);
-
-            final String randomKey = UUID.randomUUID().toString();
-            StorageReference riversRef = storageReference.child("task_images/" + randomKey);
-
-            riversRef.putFile(imageUri)
-                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                        @Override
-                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                            progressBar.setVisibility(View.INVISIBLE);
-                            Snackbar.make(findViewById(android.R.id.content), "Image Uploaded.", Snackbar.LENGTH_SHORT).show();
-
-                            riversRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-                                @Override
-                                public void onSuccess(Uri uri) {
-                                    testURI = String.valueOf(uri);
-                                    firestore.collection("tasks").document(taskUUID).update("image", testURI);
-
-                                    for (Task t: user.getTasks())
-                                    {
-                                        if (t.getTaskUUID().equals(taskUUID))
-                                        {
-                                            t.setImage(testURI);
-                                        }
-                                    }
-
-                                    Task task = new Task(testURI, nameString, timeInt, rewardInt, taskUUID);
-                                    firestore.collection("tasks").document(task.getTaskUUID()).set(task);
-                                    firestore.collection("users").document(user.getID()).update("tasks", FieldValue.arrayUnion(task));
-                                    Toast.makeText(getApplicationContext(), "Added task", Toast.LENGTH_LONG).show();
-                                    clearPage();
-//                                firestore.collection("users").document(user.getID()).set(user);
-                                }
-                            });
-                        }
-                    })
-                    .addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            progressBar.setVisibility(View.INVISIBLE);
-                            Toast.makeText(getApplicationContext(), "Failed to upload", Toast.LENGTH_SHORT).show();
-                        }
-                    })
-                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
-                        @Override
-                        public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
-                            double progressPercent = (100 * snapshot.getBytesTransferred() / snapshot.getTotalByteCount());
-                            int progress = (int) progressPercent;
-                            progressBar.setProgress(progress, true);
-                        }
-                    });
-        } catch(Exception err){
-            err.printStackTrace();
-            Toast.makeText(getApplicationContext(), "Incorrect input. Only input numbers", Toast.LENGTH_LONG).show();
-            taskTime.setText(null);
-            taskPoints.setText(null);
+        if (!nameString.equals("") && !timeString.equals("") && !rewardString.equals("")) {
+            return true;
+        } else {
+            Toast.makeText(this, "missing info", Toast.LENGTH_SHORT).show();
+            return false;
         }
     }
 
-    public void clearPage(){
+    public void addTask(View view)
+    {
+        if(formValid())
+        {
+            Integer timeInt = Integer.parseInt(timeString);
+            Integer rewardInt = Integer.parseInt(rewardString);
+
+            try {
+                taskUUID = UUID.randomUUID().toString();
+
+                final ProgressBar progressBar = new ProgressBar(this, null, android.R.attr.progressBarStyleLarge);
+                progressBar.setVisibility(View.VISIBLE);
+
+                final String randomKey = UUID.randomUUID().toString();
+                StorageReference riversRef = storageReference.child("task_images/" + randomKey);
+
+                riversRef.putFile(imageUri)
+                        .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                            @Override
+                            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                progressBar.setVisibility(View.INVISIBLE);
+                                Snackbar.make(findViewById(android.R.id.content), "Image Uploaded.", Snackbar.LENGTH_SHORT).show();
+
+                                riversRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                    @Override
+                                    public void onSuccess(Uri uri) {
+                                        testURI = String.valueOf(uri);
+                                        firestore.collection("tasks").document(taskUUID).update("image", testURI);
+
+                                        for (Task t : user.getTasks()) {
+                                            if (t.getTaskUUID().equals(taskUUID)) {
+                                                t.setImage(testURI);
+                                            }
+                                        }
+
+                                        Task task = new Task(testURI, nameString, timeInt, rewardInt, taskUUID);
+                                        firestore.collection("tasks").document(task.getTaskUUID()).set(task);
+                                        firestore.collection("users").document(user.getID()).update("tasks", FieldValue.arrayUnion(task));
+                                        Toast.makeText(getApplicationContext(), "Added task", Toast.LENGTH_LONG).show();
+                                        clearPage();
+//                                firestore.collection("users").document(user.getID()).set(user);
+                                    }
+                                });
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                progressBar.setVisibility(View.INVISIBLE);
+                                Toast.makeText(getApplicationContext(), "Failed to upload", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                            @Override
+                            public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                                double progressPercent = (100 * snapshot.getBytesTransferred() / snapshot.getTotalByteCount());
+                                int progress = (int) progressPercent;
+                                progressBar.setProgress(progress, true);
+                            }
+                        });
+            } catch (Exception err) {
+                err.printStackTrace();
+                Toast.makeText(getApplicationContext(), "ERROR", Toast.LENGTH_LONG).show();
+            }
+        }
+        else
+        {
+            Toast.makeText(getApplicationContext(), "Fill in all fields correctly", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    public void clearPage() {
         taskName.setText(null);
         taskTime.setText(null);
         taskPoints.setText(null);
@@ -221,13 +232,13 @@ public class AddTaskActivity extends AppCompatActivity
         back();
     }
 
-    public void back(){
+    public void back() {
         Intent goBackIntent = new Intent(this, AllTaskActivity.class);
         goBackIntent.putExtra("user", user);
         startActivity(goBackIntent);
     }
 
-    public void backButton(View v){
+    public void backButton(View v) {
         Intent goBackIntent = new Intent(this, AllTaskActivity.class);
         goBackIntent.putExtra("user", user);
         startActivity(goBackIntent);
